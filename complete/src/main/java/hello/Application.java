209dbe8e6800daaf09e485a061533a28726d4d71
czhifa <czhifa@sina.com>
2019-11-29 16:58:10 +0800

    [M31-1166] some update

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,12 +1,19 @@
 package hello;
-
+  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        //SpringApplication.run(Application.class, args);
+
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "9000"));
+        app.run(args);
     }
 }
